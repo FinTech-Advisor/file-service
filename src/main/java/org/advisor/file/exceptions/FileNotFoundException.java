857c9481238646d496f98094520ae90d4be43d10
rlaehddud475 <6070kjh475@naver.com>
2025-01-17 11:53:22 +0900

    .

---
+++ src/main/java/org/advisor/file/exceptions/FileNotFoundException.java
@@ -0,0 +1,12 @@
+package org.advisor.file.exceptions;
+
+
+import org.advisor.global.exceptions.CommonException;
+import org.springframework.http.HttpStatus;
+
+public class FileNotFoundException extends CommonException {
+    public FileNotFoundException() {
+        super("NotFound.file", HttpStatus.NOT_FOUND);
+        setErrorCode(true);
+    }
+}
